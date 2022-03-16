42eb7f2233e63121b651c9c9589a875ed7a07de0
mathia-gowb <Tobvins@gmail.com>
2022-03-16 10:39:54 +0200

    done with the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import {searchAndCreateElements} from './data';
 
+/* mapping through the results */
+/* then populating the dom with the found results */
 function App() {
+  const [names,setNames]=useState();
+  function handleInputChange(e){
+    /* get the input */
+    console.log(searchAndCreateElements())
+    setNames(searchAndCreateElements(e.target.value))
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="search-container">
+        <input type="text" placeholder="Type Your Search Term....." onChange={handleInputChange}/>
+        <ul className="search-results">
+            {names}
+        </ul>
+      </div>
+
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,20 @@
+const names=["Aaran","Aaren", "Aarez", "Aarman", "Aaron", "Geomer", "Geordan", "Geordie", "George", "Georgia", "Georgy", "Gerard", "Ghyll", "Giacomo", "Gian", "Giancarlo", "Gianluca", "Gianmarco", "Gideon", "Gil", "Gio", "Girijan", "Girius", "Gjan", "Glascott", "Glen", "Glenn", "Gordon", "Grady", "Graeme", "Graham", "Grahame","Hari", "Harikrishna", "Haris", "Harish", "Harjeevan", "Harjyot", "Harlee", "Harleigh", "Harley","Harvey", "Harvie", "Harvinder", "Hasan", "Haseeb", "Hashem", "Hashim", "Hassan", "Hassanali", "Hately", "Havila", "Hayden", "Haydn", "Haydon", "Haydyn", "Hcen", "Hector", "Heddle", "Heidar", "Heini", "Hendri", "Henri", "Henry", "Herbert", "Heyden", "Hiro", "Hirvaansh", "Hishaam", "Hogan", "Honey", "Hong", "Hope", "Hopkin", "Hosea", "Howard", "Howie", "Hristomir", "Hubert", "Hugh", "Hugo", "Humza", "Hunter", "Husnain", "Hussain","Jac", "Jace", "Jack", "Jacki", "Jackie", "Jack-James", "Jackson", "Jacky", "Jacob", "Jacques", "Jad", "Jaden", "Jadon", "Jadyn", "Jae", "Jagat", "Jago", "Jaheim", "Jahid", "Jahy", "Jai", "Jaida", "Jaiden", "Jaidyn", "Jaii","Jameel", "Jameil", "James", "James-Paul", "Jamey", "Jamie", "Jan", "Jaosha", "Jardine", "Jared", "Jarell", "Jarl", "Jarno", "Jarred", "Jarvi", "Jasey-Jay", "Jasim", "Jaskaran", "Jason", "Jasper", "Jaxon", "Jaxson", "Jay", "Jaydan", "Jaydyn", "Jayhan", "Jay-Jay", "Jayke", "Jaymie", "Jayse", "Jayson", "Jaz", "Jazeb", "Jazib", "Jazz", "Jean", "Jean-Lewis", "Jean-Pierre", "Jebadiah", "Jed", "Jedd", "Jedidiah", "Jeemie", "Jeevan", "Jeffrey", "Jensen", "Jenson", "Jensyn", "Jeremy", "Jerome", "Jeronimo","Jiao", "Jimmy", "Jincheng", "JJ", "Joaquin", "Joash", "Jock", "Jody", "Joe"];
+export function searchAndCreateElements(value){
+    let results;
+    if(value){
+        let testValue=new RegExp(`${value}`,'i');
+        console.log(testValue);
+        let foundNames=names.filter(letters=>testValue.test(letters))
+        //
+        console.log(value,<li>{value}</li>)
+        results=foundNames.map(name=><li className="results-item">Suggested Name Is {/* {name.replace(value,<span className="shade">{value}</span>)} */} </li>)
+
+    }
+    if(results){
+        console.log(results)
+        return results
+    }else{
+        return [<li>There is nothing to display</li>]
+    }
+   
+}
